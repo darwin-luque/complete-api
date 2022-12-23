56f73ba68a9bd79ff9f723b0d25bc4b3d90edf06
Darwin Luque <darwin.luque.98@gmail.com>
2022-12-23 10:39:35 -0600

    chore: create product concept

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { IProduct } from './types';
 
 @Injectable()
 export class AppService {
+  private readonly products: Map<string, IProduct> = new Map();
+
   getHello(): string {
     return 'Hello World!';
   }
