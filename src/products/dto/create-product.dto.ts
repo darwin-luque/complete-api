e52e7c5f221fa7608cf315c2a46494cc1d21e432
Darwin Luque <darwin.luque.98@gmail.com>
2022-12-23 11:14:58 -0600

    chore: implement validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
   name: string;
+
+  @IsNumber()
+  @Min(0)
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsUrl()
   image?: string;
 }
